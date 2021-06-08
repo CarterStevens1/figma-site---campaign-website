57ebf5e8b861ecd61c6e66fd221da394ff2e6ad2
CarterStevens1 <CarterStevens1@users.noreply.github.com>
2021-06-08 11:54:52 +0100

    remove function

--- script.js
+++ script.js
@@ -7,17 +7,3 @@ toggle between hiding and showing the dropdown content */
 nav.addEventListener("click", () => {
   document.querySelector(".dropdown").classList.toggle("active");
 });
-
-// Close the dropdown if the user clicks outside of it
-window.onclick = function (event) {
-  if (!event.target.matches(".dropbtn")) {
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains("show")) {
-        openDropdown.classList.remove("show");
-      }
-    }
-  }
-};
