1282b71e6f143c420d158a6e5406828d7374ed3b
CarterStevens1 <CarterStevens1@users.noreply.github.com>
2021-06-15 20:31:12 +0100

    removed jquery as fixed issue with max-width

--- script.js
+++ script.js
@@ -7,9 +7,3 @@ toggle between hiding and showing the dropdown content */
 nav.addEventListener("click", () => {
   document.querySelector(".dropdown").classList.toggle("active");
 });
-function checkWidth() {
-  if ($(window).width() < 950) $(".testimonials").addClass("inner-container");
-  else $(".testimonials").removeClass("inner-container");
-}
-
-$(window).resize(checkWidth);
