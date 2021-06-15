b67568f92030d9fe759a59650f306b164e9fe26e
CarterStevens1 <CarterStevens1@users.noreply.github.com>
2021-06-15 18:55:43 +0100

    full scaling

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 "use strict";
-
+const testimonials = document.querySelector(".testimonials");
 const nav = document.querySelector(".nav-button");
-/* When the user clicks on the button, 
+/* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
 
 nav.addEventListener("click", () => {
   document.querySelector(".dropdown").classList.toggle("active");
 });
+function checkWidth() {
+  if ($(window).width() < 950) $(".testimonials").addClass("inner-container");
+  else $(".testimonials").removeClass("inner-container");
+}
+
+$(window).resize(checkWidth);
